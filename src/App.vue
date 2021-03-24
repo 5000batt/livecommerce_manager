c2dc8680cd848febca5f7514e9e25227f3816883
5000batt <5000batt@gmail.com>
2021-03-24 15:59:06 +0900

    twitch + broadcastadd

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,52 @@
 <template>
-<v-app>
-    <v-app-bar color="primary" dark>
-    <!-- -->
-                          <!--click -> e.stopPropagation-->
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    
+  <v-app>
+    <v-app-bar color="primary" dark app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-app-bar-title>라이브 커머스 관리자</v-app-bar-title>
-    
-  </v-app-bar>
-
-  <v-navigation-drawer v-model="drawer">
-    <!-- -->
-    <v-list dense>
-      <!-- <v-subheader>MENU</v-subheader> -->
-      <v-list-item-group v-model="selectedItem" color="primary">
-        <v-list-item v-for="(item, i) in items" :key="i" @click="navigateTo(item)">
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-  </v-navigation-drawer>
-
-  <!-- Sizes your content based upon application components -->
-  <v-main>
-
-    <!-- Provides the application the proper gutter -->
-    <v-container>
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-  </v-main>
-
-  <v-footer>
-    <!-- -->
-  </v-footer>
-</v-app>  
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            @click="navigateTo(item)"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 <script>
-  export default {
-    name: 'App',
-    data: () => ({
-      drawer: true, // drawer의 기본 값
-      selectedItem: 0,
-      items: [
-        /* https://cdn.materialdesignicons.com/5.4.55/ */
-        { text: '방송관리', icon: 'mdi-message-video', path: '/' },
-        { text: '녹화관리', icon: 'mdi-record-rec', path: '/contacts' },
-        { text: '방송가이드', icon: 'mdi-television-guide', path: '/feeds' },
-      ],     
-    }),
-    methods: {
-      navigateTo(item) {
-        /* https://router.vuejs.org/kr/guide/essentials/navigation.html */
-        // 현재 경로와 다르면
-        if(this.$route.path != item.path){
-          // 라우터에 경로 추가
-          this.$router.push(item.path);
-        }
+export default {
+  name: "App",
+  data: () => ({
+    drawer: true,
+    selectedItem: 0,
+    items: [
+      { text: "방송관리", icon: "mdi-message-video", path: "/" },
+      { text: "녹화관리", icon: "mdi-record-rec", path: "/recording" },
+      { text: "방송가이드", icon: "mdi-television-guide", path: "/guide" },
+    ],
+  }),
+  methods: {
+    navigateTo(item) {
+      if (this.$route.path != item.path) {
+        this.$router.push(item.path);
       }
-    }
-  }
+    },
+  },
+};
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,121 @@
 <template>
-  <twitch-player
-    :channel="channel"
-  ></twitch-player>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="8">
+          <div id="twitch-embed"></div>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-dialog v-model="dialog" persistent max-width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                dark
+                v-bind="attrs"
+                v-on="on"
+                absolute
+                right
+              >
+                방송 정보추가
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">방송 정보</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="6">
+                      <v-img
+                        height="100%"
+                        src="https://picsum.photos/id/11/500/300"
+                      ></v-img>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="6">
+                      <v-row no-gutters>
+                        <v-col cols="12">
+                          <v-text-field label="방송제목"></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-select :items="items" label="카테고리"></v-select>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-autocomplete label="상품이름"></v-autocomplete>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-file-input label="대표이미지"></v-file-input>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field label="가격"></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="dialog = false">
+                  저장
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="dialog = false">
+                  수정
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="dialog = false">
+                  삭제
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <!-- <v-col cols="12" sm="4">
+            <v-btn color="primary"></v-btn>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-btn color="primary"></v-btn>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-btn color="primary"></v-btn>
+          </v-col> -->
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-  import TwitchPlayer from 'vue-twitch-player';
+// import TwitchPlayer from "vue-twitch-player";
 
-  export default {
-    // ...
-    components: {
-      TwitchPlayer,
-    },
-    data() {
-      return {
-        channel: 'wjddnjs813',
-      };
-    },
-    // ...
-  };
+export default {
+  // ...
+  components: {
+    // TwitchPlayer,
+  },
+  data() {
+    return {
+      // channel: "wjddnjs813",
+      dialog: false,
+      items: ["광장시장", "의정부시장"],
+    };
+  },
+  mounted() {
+    // const library = document.createElement("script");
+    // library.src = "https://embed.twitch.tv/embed/v1.js";
+    // document.body.appendChild(library);
+
+    const twitch = document.createElement("script");
+    twitch.innerHTML = `
+      new Twitch.Embed("twitch-embed", {
+          width: 854,
+          height: 480,
+          channel: "wjddnjs813",
+          // video: "951261050",
+          // only needed if your site is also embedded on embed.example.com and othersite.example.com
+          parent: ["embed.example.com", "othersite.example.com"]
+        });
+      `;
+    document.body.appendChild(twitch);
+  },
+  // ...
+};
 </script>
